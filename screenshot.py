6ebc72338044102ccbc0c92749420cf55ac01467
D1al-T0ne <jesseason@mini.lan>
2021-09-24 20:51:27 -0400

    Changed how filename is created

--- screenshot.py
+++ screenshot.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
 import os
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 os.makedirs("screenshots",exist_ok=True)
 
 chrome_options = Options()
-chrome_options.add_argumetn("--headless")
+chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
 
-with open("test.txt") as t:
-  urls = t.readlines()
-  for url in urls:
-    name = url.strip()
-    filename = name.replace(".com","")
-    driver.get(f"https://{url}")
-    driver.get_screenshot_as_file(f"./screenshots/{filename}.png")
-    print(f"[+]{name} captured!")
+def screenshot(domain):
+    filename = os.path.splitext(domain)[0]+".png" 
+    driver.get(f"https://{domain}")
+    driver.get_screenshot_as_file(f"./screenshots/{filename}")
+
+def main():
+	
+	screenshot(sys.argv[1])
+#with open("test.txt") as t:
+#  urls = t.readlines()
+#  for url in urls:
+#    name = url.strip()
+#    print(f"[+]{name} captured!")
+if __name__ == "__main__":
+	main()
