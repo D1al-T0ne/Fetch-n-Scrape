fa1ce47013ea2ac19c44c5da45372379f9fc493b
Jess <d1al.t0ne@outlook.com>
2022-01-20 10:37:25 -0500

    Added exception handling

--- statuscode.py
+++ statuscode.py
@@ -7,11 +7,18 @@ import concurrent.futures
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15"}
 
 def get_status(url):
-    with httpx.Client(headers=headers) as client:
-        r = client.get(url)
-        print(f"[{r.status_code}] - {url}")
-    
+    with httpx.Client(headers=headers,verify=False) as client:
+    	try:
+            r = client.get(f"https://{url}")
+            print(f"[{r.status_code}] - {url}")
+        except httpx.ConnectTimeout:
+            print(f"[*] Hit timeout limit when connecting to {url}")
+        except httpx.NetworkError:
+            print(f"[*] A netword error occured when conntecting to {url}")
+        except KeyboardInterrupt:
+            print("[*] Stopping ...") 
 
+    
 def main():
 
     parser = argparse.ArgumentParser()
