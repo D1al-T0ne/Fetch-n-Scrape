df6e02b339c1770140792055585c0c9639c41b31
Jess <d1al.t0ne@outlook.com>
2022-01-29 08:37:31 -0500

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python
 
+import argparse
 import re
 
+def pattern(filename):
 # Import pattern wordlist
-with open("./Patterns/rce.txt") as r:
-	patterns_list = [pattern.strip() for pattern in r]
-	patterns = re.compile(r'\b(?:%s)\b' % '|'.join(patterns_list))
+    with open(f"./Patterns/{filename}") as r:
+        patterns_list = [pattern.strip() for pattern in r]
+        patterns = re.compile(r'\b(?:%s)\b' % '|'.join(patterns_list))
+        return patterns
 
+def main():
 
-with open("raw-gau-data.txt") as g:
-	lines = g.readlines()
-	for line in lines:
-		if re.search(patterns, line):
-			print(line, end="")
+    parser = argparse.ArgumentParser()
 
-#https://stackoverflow.com/questions/6750240/how-to-do-re-compile-with-a-list-in-python
-#https://stackoverflow.com/questions/4785244/search-a-text-file-and-print-related-lines-in-python
+    parser.add_argument("-i", "--input", help="File of URLs to be parsed.", required =True)
+    parser.add_argument("-p", "--pattern", help="Pattern to look for.", required =True)
+
+    args = parser.parse_args()
+
+    with open(args.input) as g:
+        results = (line.strip() for line in g.readlines() if re.search(pattern(args.pattern), line))
+        for result in results:
+            print(result, sep="")
+
+if __name__ == "__main__":
+    main()
